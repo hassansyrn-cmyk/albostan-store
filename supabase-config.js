fbdf85f28f12e2a87283112bd5972714b31853f2
hassansyrn-cmyk <hassan.syrn@gmail.com>
2026-06-25 00:57:37 +0400

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+// ملف: supabase-config.js
+
+const SUPABASE_URL = 'https://essjjrirzthhdujizoas.supabase.co';
+
+const SUPABASE_ANON_KEY = 'sb_publishable_2cmZdYgbId1mew954vRsLw_RnsJh2BS';
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
