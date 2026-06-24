0602fedae03f7e5a343767e7fcaf0c88373dde76
hassansyrn-cmyk <hassan.syrn@gmail.com>
2026-06-25 01:46:13 +0400

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,11 @@
+
 // ملف: supabase-config.js
 
+// 1. رابط مشروعك (Project URL)
 const SUPABASE_URL = 'https://essjjrirzthhdujizoas.supabase.co';
 
-const SUPABASE_ANON_KEY = 'sb_publishable_2cmZdYgbId1mew954vRsLw_RnsJh2BS';
+// 2. مفتاح الواجهة الأمامية (anon public key)
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzc2pqcmlyenRoaGR1aml6b2FzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzMTU3ODAsImV4cCI6MjA5Nzg5MTc4MH0.H-m33ZmSjv2ZrEIK2gpCg55a-xJ4ox0w4OCOraO2oQE';
 
+// تهيئة الاتصال بـ Supabase
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
